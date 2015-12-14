3e3e507c8771b9196e3076aca1fbff719408382e
Denis <denis@clientlan-1-100.mlzone.ru>
2015-12-14 10:27:24 +0300

    init

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -9,37 +9,7 @@ var User = new Schema({
 		type: String,
 		required: true,
 		unique: true
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true
-	},
-	phone: {
-		type: String,
-		unique: true
-	},
-	created: {
-		type: Date,
-		"default": Date.now
-	},
-	permissions: {
-		type: String
-	},
-	resetPasswordToken: {
-    	type: String
-	},
-  	resetPasswordExpires: {
-    	type: Date,
-    	"default": new Date().setDate(new Date().getDate() + 90)
-  	},
-  	resetTime: {
-  		type: String
-  	},
-  	newbee: {
-  		type: Boolean,
-  		"default": true 
-  	}
+	}
 });
 
 User.plugin(passportLocalMongoose);
--- gulpfile.js
+++ gulpfile.js
@@ -2,35 +2,27 @@ var gulp 		= require('gulp'),
 	concat 		= require('gulp-concat'),
 	plumber 	= require('gulp-plumber'),
 	server 		= require('tiny-lr')(),
-	refresh 	= require('gulp-livereload'),
 	minifyCss  	= require('gulp-minify-css'),
 	notify 		= require('gulp-notify'),
 	nodemon 	= require('gulp-nodemon'),
 	jshint 		= require('gulp-jshint'),
 	watch 		= require('gulp-watch'),
-	livereload 	= require('gulp-livereload'),
+	browserSync = require('browser-sync').create(),
 	lrPort 		= 35729;
 
 var paths = {
-  styles: ['./public/styles/*.css'],
-  scripts: [
-	'./public/scripts/**/*.js'
-  ],
-  html: [
-	'./public/views/**/*.html'
-  ],
-
-  server: {
-	js: ['./app/**/*.js']
-  }
+  styles: 	['./public/styles/*.css'],
+  scripts: 	['./public/scripts/**/*.js'],
+  html: 	['./public/views/**/*.html'],
+  server: 	['./app/**/*.js']
 };
 
-gulp.task('serve', function(){
+gulp.task('serve', function() {
 	nodemon({'script': 'server.js'});
 });
 
-gulp.task('lint', function(){
-	return gulp.src(paths.scripts)
+gulp.task('lint', function() {
+	return gulp.src(paths.scripts, paths.server)
 	.pipe(plumber())
 	.pipe(jshint())
 	.pipe(jshint.reporter('default'))
@@ -38,12 +30,10 @@ gulp.task('lint', function(){
 });
 
 gulp.task('scripts', function(){
-	return gulp.src(paths.scripts)
+	return gulp.src(paths.scripts, paths.server)
 	.pipe(plumber())
 	.pipe(concat('main.js'))
 	.pipe(gulp.dest('./public/build/scripts'))
-	.pipe(refresh(server))
-	.pipe(livereload())
 	.pipe(notify({message: 'JS concated'}));
 });
 
@@ -52,34 +42,39 @@ gulp.task('minifyCss', function(){
 	.pipe(plumber())
 	.pipe(minifyCss())
 	.pipe(gulp.dest('./public/build/styles'))
-	.pipe(refresh(server))
-	.pipe(livereload())
 	.pipe(notify({message: 'Css minify done'}));
 });
 
-gulp.task('html', function(){
-  return gulp.task('html', function(){
+gulp.task('html', function() {
+  return gulp.task('html', function() {
 	gulp.src(paths.html)
-		.pipe(refresh(server))
-		.pipe(livereload(server))
 		.pipe(notify({message: 'Views refreshed'}));
   	});
 });
 
-gulp.task('build', ['minifyCss', 'scripts', 'lint', 'html']);
+gulp.task('build', ['minifyCss', 'scripts', 'lint']);
 
-gulp.task('lr', function(){
-	server.listen(lrPort, function(err){
+gulp.task('lr', function() {
+	server.listen(lrPort, function(err) {
 		if(err) {
 			return console.error(err);
 		}
 	});
 });
 
-gulp.task('watch', function(){
-	gulp.watch(paths.html, ['html']);
-	gulp.watch(paths.scripts, ['lint', 'scripts']);
-	gulp.watch(paths.styles, ['minifyCss']);
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./public"
+        }
+    });
 });
 
-gulp.task('default', ['build', 'lr', 'serve', 'watch']);
+gulp.task('watch', function() {
+	gulp.watch(paths.html, ['html']).on('change', browserSync.reload);
+	gulp.watch(paths.scripts, ['lint', 'scripts']).on('change', browserSync.reload);
+	gulp.watch(paths.styles, ['minifyCss']).on('change', browserSync.reload);
+	gulp.watch(paths.server, ['lint']).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['build', 'lr', 'serve', 'browser-sync', 'watch']);
